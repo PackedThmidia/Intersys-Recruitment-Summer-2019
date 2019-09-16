cdd9facef4de63e96b87f334a8956c94744614c7
Paweł Rumiński <specu1997@gmail.com>
2019-09-16 16:22:31 +0200

    Initial commit

---
+++ Task_2_Palindrome.js
@@ -0,0 +1,33 @@
+// Write a program that takes a string as input and prints whether this string is a true palindrome or not. A true palindrome is a string such that if you remove all characters except for letters (uppercase and lowercase), the string is equal if read from left to right and from right to left. If the inputted string is a true palindrome, the message YES (uppercase required) is displayed else NO is displayed.
+
+// Notice that lowercase letters are equal to uppercase letters and vice-versa.
+
+// Example
+
+// Case 1:
+
+// For input provided as follows:
+
+// abut-1-tuba
+
+// Output of the program will be: 
+
+// YES 
+
+// Description:
+
+// If we remove the bad characters 1 and -, we get the string “abuttuba” which is a palindrome.
+
+// Case 2:
+
+// For the input provided as follows:
+
+// @allula 
+
+// Output of the program will be:
+
+// NO
+
+// Description:
+
+// After removing the bad characters we get “allula” which is not a palindrome.
---
+++ Task_7_Fix_age.js
@@ -0,0 +1,13 @@
+// Write a function in JS fixage that receives an array of integers. The integers represent ages between 0 and 100. The function is required to join all the integers that are between 18 and 60 into a string separated with a comma and returns it. If there is no integer between 18 and 60, then the function returns "NA".
+
+// For example:
+
+// fixage([5,15,25,78,59,45]) returns 25,59,45
+
+// and
+
+// fixage([18,3,30,22,11,60]) returns 18,30,22,60
+
+// and 
+
+// fixage([1,3,3,2,11,6]) returns NA
